3fab440258d89827c2abe8b79dac5ec15312811c
TaeHeon-Lee <taeheon714@gmail.com>
2021-08-16 21:16:43 +0900

    Correct Commentary

--- level1/recommendation_for_new_ID.py
+++ level1/recommendation_for_new_ID.py
@@ -8,7 +8,7 @@ def solution(new_id):
     new_id = new_id.lower()                                 # 1 단계
     new_id = re.sub('[^a-z0-9-_.]', '', new_id)             # 2 단계 (^은 "not"의미, 리스트로 묶을 경우 그룹으로 처리 가능)
     new_id = re.sub('\.+', '.', new_id)                     # 3 단계 (.앞에 \를 붙일 경우, 해당 문자 매칭만 치환, *은 0번 이상 매칭, +는 1번 이상 매칭)
-    new_id = re.sub('^[.]|[.]$', '', new_id)                # 4 단계 ($은 마지막을 의미, |는 "or" 의미)
+    new_id = re.sub('^[.]|[.]$', '', new_id)                # 4 단계 (^은 제일 앞을 의미, $은 마지막을 의미, |는 "or" 의미)
     new_id = 'a' if len(new_id) == 0 else new_id            # 5 단계
     new_id = new_id[:15] if len(new_id) > 15 else new_id    # 6 단계
     new_id = re.sub('^[.]|[.]$', '', new_id)
