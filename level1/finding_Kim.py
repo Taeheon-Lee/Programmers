10903fa4c61d2b8d774df8ac00db522a07d73b13
TaeHeon-Lee <taeheon714@gmail.com>
2021-08-18 00:41:46 +0900

    finding Kim

---
+++ level1/finding_Kim.py
@@ -0,0 +1,7 @@
+"서울에서 김서방 찾기"
+
+# 문제 링크 "https://programmers.co.kr/learn/courses/30/lessons/12919"
+
+def solution(seoul):
+    answer = "김서방은 " + str(seoul.index("Kim")) + "에 있다"  # index() 함수는 특정 값이 리스트 어디에 있는지 확인하는 함수
+    return answer
