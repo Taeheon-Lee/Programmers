6f52124daf125a319c43b524a01d673cb975b23d
TaeHeon-Lee <taeheon714@gmail.com>
2021-08-18 01:04:31 +0900

    SooBak

---
+++ level1/SooBak.py
@@ -0,0 +1,9 @@
+"수바수박수박수박수박수?"
+
+# 문제 링크 "https://programmers.co.kr/learn/courses/30/lessons/12922"
+
+def solution(n):
+    answer = "수박" * (n // 2) # 문자열 반복을 연산자를 이용하여 반복
+    if n % 2 != 0:
+        answer += "수"
+    return answer
