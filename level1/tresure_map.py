c7abeacbf9c90345ba2e126bead0d00bfd272a42
TaeHeon-Lee <taeheon714@gmail.com>
2021-08-17 19:43:54 +0900

    tresure map

---
+++ level1/tresure_map.py
@@ -0,0 +1,10 @@
+"보물 지도"
+
+# 문제 링크 "https://programmers.co.kr/learn/courses/30/lessons/17681"
+
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        tmp = bin(arr1[i]|arr2[i])[2:].replace("1","#").replace("0"," ")    # & 연산은 두개의 정수를 2진수로 변환하여 and 매칭, | 연산은 or 매칭
+        answer.append((n-len(tmp))*" "+tmp)                                 # 결과 값이 지도의 길이보다 작을 경우, 빈 공간을 " "을 추가
+    return answer
