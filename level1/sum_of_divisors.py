214cbc3165181dac240d2020a68af522b040a765
TaeHeon-Lee <taeheon714@gmail.com>
2021-08-19 01:07:42 +0900

    sum of divisors

--- level1/sum_of_divisors.py
+++ level1/sum_of_divisors.py
@@ -1,12 +1,10 @@
-"약수의 개수와 덧셈"
+"약수의 합"
 
-# 문제 링크 "https://programmers.co.kr/learn/courses/30/lessons/77884"
+# 문제 링크 "https://programmers.co.kr/learn/courses/30/lessons/12928"
 
-def solution(left, right):
+def solution(n):
     answer = 0
-    for i in range(left, right+1):  # 범위 탐색
-        if i % (i**0.5) != 0:       # 제곱근이 존재하고 나누어 떨어지지 않을 경우, 개수는 반드시 짝수이기 때문에 덧셈
-            answer += i
-        else:                       # 떨어지는 경우, 개수는 반드시 홀수이기 때문에 뺄셈
-            answer -= i
+    for i in range(1, n+1):
+        if n % i == 0:      # 약수일 경우
+            answer += i     # 더하기
     return answer
